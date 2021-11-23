990f850a6fd5a9c0a1f61b575af721360df15bc6
TomasHurtado <tomas221298@gmail.com>
2021-11-23 09:47:36 -0300

    mercado liebre

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app=express();
 app.use(express.static("public"));
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT ||3000,()=>{
     console.log("servidor andando")//recursos
 });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get('/register.html', (req, res) => {
+  res.sendFile(__dirname + "/views/register.html")
+})
+app.get('/login.html', (req, res) => {
+  res.sendFile(__dirname + "/views/login.html")
 })
 
